cfea0b812347345941d9827e64d57486101b2464
juyangjin <wndid2008@gmail.com>
2026-08-25 22:36:33 +0900

    chore: 기본 구조 스켈레톤 추가

---
+++ src/main/java/com/team3/common/package-info.java
@@ -0,0 +1,1 @@
+package com.team3.common;
---
+++ src/test/java/com/team3/BackendApplicationTests.java
@@ -0,0 +1,17 @@
+package com.team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude="
+				+ "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
+				+ "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
