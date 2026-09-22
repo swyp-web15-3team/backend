ca961b512a8616b71bd26f840794015bf51e67ea
juyangjin <wndid2008@gmail.com>
2026-09-22 14:15:19 +0900

    feat: 큐레이션 목록을 검색형 페이징으로 조회한다

--- src/main/java/com/team3/curation/CurationRepository.java
+++ src/main/java/com/team3/curation/CurationRepository.java
@@ -1,6 +1,10 @@
 package com.team3.curation;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CurationRepository extends JpaRepository<Curation, Long> {
+
+    Optional<Curation> findFirstByOrderByIdAsc();
 }
