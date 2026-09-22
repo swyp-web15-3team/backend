7db9e278cde461ee79ea6fce0db811794e1d7e6d
juyangjin <wndid2008@gmail.com>
2026-09-22 13:55:28 +0900

    feat: 플래너 항목 단건 삭제 API 추가

--- src/main/java/com/team3/planner/PlannerController.java
+++ src/main/java/com/team3/planner/PlannerController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,14 @@ public class PlannerController {
         @AuthenticationPrincipal Jwt jwt,
         @RequestBody AddPlannerItemsRequest request) {
         return ApiResponse.of(planners.addItems(Long.valueOf(jwt.getSubject()), request));
+    }
+
+    @DeleteMapping("/items/{plannerItemId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteItem(
+        @AuthenticationPrincipal Jwt jwt,
+        @PathVariable Long plannerItemId) {
+        planners.deleteItem(Long.valueOf(jwt.getSubject()), plannerItemId);
     }
 
     @DeleteMapping("/items")
