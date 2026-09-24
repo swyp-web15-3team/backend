97c66ec07940f9a224fadc6d2a28047bcbb21ada
juyangjin <wndid2008@gmail.com>
2026-09-24 13:52:30 +0900

    fix: 배포 프론트 Origin CORS 허용

--- src/main/java/com/team3/security/CorsConfig.java
+++ src/main/java/com/team3/security/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
             .allowedOrigins("http://sulchedule-server.sunghoyaaa.com", "https://sulchedule-server.sunghoyaaa.com",
-                "http://localhost:3000")
+                "https://sulchedule.sunghoyaaa.com", "http://localhost:3000")
             .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("Authorization", "Content-Type", "Accept");
     }
