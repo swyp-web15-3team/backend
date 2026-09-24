9d6849b15f501296b71c5e9686048348af5cc518
mobius29 <sagjin0000@naver.com>
2026-09-24 23:09:56 +0900

    refactor: move collection errors to collection domain

--- src/main/java/com/team3/collection/exception/DefaultCollectionImmutableException.java
+++ src/main/java/com/team3/collection/exception/DefaultCollectionImmutableException.java
@@ -1,11 +1,10 @@
 package com.team3.collection.exception;
 
 import com.team3.common.exception.CustomException;
-import com.team3.common.exception.ErrorCode;
 
 public class DefaultCollectionImmutableException extends CustomException {
 
     public DefaultCollectionImmutableException() {
-        super(ErrorCode.DEFAULT_COLLECTION_IMMUTABLE);
+        super(CollectionErrorCode.DEFAULT_COLLECTION_IMMUTABLE);
     }
 }
