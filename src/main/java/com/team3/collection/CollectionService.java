1781b50eb946573561aa8d067d25ef65081c96cb
juyangjin <wndid2008@gmail.com>
2026-09-25 11:56:08 +0900

    feat: 관심 그룹 위스키 복사 요청 DTO 추가

--- src/main/java/com/team3/collection/CollectionService.java
+++ src/main/java/com/team3/collection/CollectionService.java
@@ -9,6 +9,7 @@ import com.team3.collection.dto.CollectionsResponse;
 import com.team3.collection.exception.CollectionNotFoundException;
 import com.team3.collection.exception.DefaultCollectionImmutableException;
 import com.team3.collection.exception.DuplicateCollectionNameException;
+import com.team3.collection.exception.SameCollectionCopyException;
 import com.team3.collection.exception.SameCollectionMoveException;
 import com.team3.collection.exception.WhiskyNotFoundException;
 import com.team3.whisky.WhiskyRepository;
@@ -119,6 +120,24 @@ public class CollectionService {
         List<CollectionWhisky> memberships = collectionWhiskies
             .findAllByCollectionIdAndWhiskyIdIn(collectionId, uniqueWhiskyIds);
         collectionWhiskies.deleteAllInBatch(memberships);
+        for (CollectionWhisky membership : memberships) {
+            Long whiskyId = membership.whiskyId();
+            if (!collectionWhiskies.existsByCollectionIdAndWhiskyId(targetCollectionId, whiskyId)) {
+                collectionWhiskies.save(new CollectionWhisky(targetCollectionId, whiskyId));
+            }
+        }
+    }
+
+    public void copyWhiskies(Long userId, Long collectionId, Long targetCollectionId, List<Long> whiskyIds) {
+        if (collectionId.equals(targetCollectionId)) {
+            throw new SameCollectionCopyException();
+        }
+        findOwnedCollection(userId, collectionId);
+        findOwnedCollection(userId, targetCollectionId);
+        Set<Long> uniqueWhiskyIds = new LinkedHashSet<>(whiskyIds);
+        assertWhiskiesExist(uniqueWhiskyIds);
+        List<CollectionWhisky> memberships = collectionWhiskies
+            .findAllByCollectionIdAndWhiskyIdIn(collectionId, uniqueWhiskyIds);
         for (CollectionWhisky membership : memberships) {
             Long whiskyId = membership.whiskyId();
             if (!collectionWhiskies.existsByCollectionIdAndWhiskyId(targetCollectionId, whiskyId)) {
